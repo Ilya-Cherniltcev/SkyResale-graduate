e4fc2bb0769aaaeabc31f3f70f1da97867a3aba9
Андрей <gosudarev_a@mail.ru>
2022-12-16 14:11:21 +0300

    Прописана зависимость liquibase-core, прописан путь в пропертиз, создан changelog-master и накидал немного скриптов

---
+++ src/main/resources/liquibase/scripts/migrations.sql
@@ -0,0 +1,42 @@
+CREATE TABLE users
+(
+    id         serial       NOT NULL PRIMARY KEY,
+    first_name varchar(255) NOT NULL,
+    last_name  varchar(255) NOT NULL,
+    phone      varchar(255) NOT NULL,
+    email      varchar(255) NOT NULL,
+    password   varchar(255) NOT NULL,
+    role       varchar(255) NOT NULL DEFAULT 'USER'
+
+);
+
+CREATE TABLE images
+(
+    pk         serial       NOT NULL PRIMARY KEY,
+    data       BYTEA,
+    file_path  varchar(255) NOT NULL,
+    file_size  integer      NOT NULL,
+    media_type varchar(255) NOT NULL
+
+);
+CREATE TABLE ads
+(
+    pk          serial       NOT NULL PRIMARY KEY,
+    price       integer      NOT NULL,
+    title       varchar(255) NOT NULL,
+    description varchar(255) NOT NULL,
+    image_id    bigint       NOT NULL REFERENCES images (pk),
+    author_id   bigint       NOT NULL REFERENCES users (id) --Как вариант, ведь автор должен быть
+);
+
+CREATE TABLE comments --Или как-то ещё назвать
+(
+    pk         serial    NOT NULL PRIMARY KEY,
+    author_id  bigint    NOT NULL REFERENCES users (id),
+    ads_pk     bigint    NOT NULL REFERENCES ads (pk),
+    created_at timestamp NOT NULL,
+    text       text      NOT NULL
+
+);
+
+

c1034817e44651de2b826c6b0ba43fdc01d496f5
Ilya-Cherniltcev <cherniltcev@mail.ru>
2023-01-17 22:44:31 +0500

    Test of update method of UserController (doesn't work)

---
+++ src/test/java/ru/skypro/homework/ConstantsForTests.java
@@ -0,0 +1,11 @@
+package ru.skypro.homework;
+
+public class ConstantsForTests {
+    public static final String DTO_IMAGE_STRING1 = "image1";
+    public static final String DTO_IMAGE_STRING2 = "image2";
+    public static final String[] DTO_IMAGE_ARRAY = {DTO_IMAGE_STRING1, DTO_IMAGE_STRING2};
+
+    public static final String DEFAULT_LOGIN = "user@gmail.com";
+    public static final String DEFAULT_PASSWORD = "password";
+
+}
